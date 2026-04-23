36b07529be139adca85115441c6da0288881e883
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-04-23 09:14:03 +0600

    new one

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -1,28 +1,43 @@
-import { useState } from 'react'
-import { searchMovies } from '../services/api'
+import { use, useEffect, useState } from 'react';
+import { boardMovies, searchMovies } from '../services/api';
+import { Link, useNavigate } from 'react-router-dom';
+import { addToFavorites, getFavorites, removeFromFavorites } from '../services/favorites';
+
+type Movie = {
+    id: string;
+    originalTitle: string;
+    primaryImage?: {
+        url: string;
+    }
+}
+
 
 export default function Home() {
-  const [query, setQuery] = useState('')
-  const [movies, setMovies] = useState<any[]>([])
+ const [query, setQuery] = useState('');
+ const [movies, setMovies] = useState([]);
 
-  return (
-    <div>
-      <input value={query} onChange={e => setQuery(e.target.value)} />
-      
-      <button onClick={() =>
-        searchMovies(query).then(d => setMovies(d.titles))
-      }>
-        Search
-      </button>
+ const handleSearch = () => {
+  searchMovies(query).then(data => {
+   setMovies(data.titles);
+  });
+ };
 
-      <div className='grid grid-cols-4 gap-2'>
-        {movies.map(m => (
-          <div className='p-2' key={m.id}>
-            <img src={m.primaryImage?.url} alt={m.originalTitle} />
-            <h3>{m.originalTitle}</h3>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+ return (
+  <div>
+   <input
+    type='text'
+    value={query}
+    onChange={e => setQuery(e.target.value)}
+   />
+   <button onClick={handleSearch}>Search</button>
+   <div className='grid grid-cols-4 gap-2'>
+    {movies.map(movie => (
+     <div className='p-2'>
+      <img src={movie?.primaryImage?.url} alt='НАЗВАНИЕ' />
+      <h3>{movie.originalTitle}</h3>
+     </div>
+    ))}
+   </div>
+  </div>
+ );
 }
